72ca26725467a64ee6690164e0157851dbdaa0a9
isura <isura@wso2.com>
2018-02-05 20:49:27 +0530

    Adding PIIController sample

---
+++ consent-mgt/piicontroller/org.wso2.carbon.identity.piicontroller/src/main/java/org/wso2/carbon/identity/piicontroller/ConsentConstants.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations und
+ */
+package org.wso2.carbon.identity.piicontroller;
+
+/**
+ * This class is used to define the constants used in consent management module.
+ */
+public class ConsentConstants {
+
+    public static final String PII_CONTROLLER = "piiController";
+    public static final String CONTACT = "contact";
+    public static final String EMAIL = "email";
+    public static final String PHONE = "phone";
+    public static final String ON_BEHALF = "onBehalf";
+    public static final String PII_CONTROLLER_URL = "piiControllerUrl";
+    public static final String ADDRESS_COUNTRY = "addressCountry";
+    public static final String ADDRESS_LOCALITY = "addressLocality";
+    public static final String ADDRESS_REGION = "addressRegion";
+    public static final String POST_OFFICE_BOX_NUMBER = "postOfficeBoxNumber";
+    public static final String POSTAL_CODE = "postalCode";
+    public static final String STREET_ADDRESS = "streetAddress";
+    public static final String PUBLIC_KEY = "publicKey";
+
+    public static final String PII_CONTROLLER_CONNECTOR_NAME = "pii-controller";
+    public static final String PII_CONTROLLER_CONNECTOR_FAMILY_NAME = "PII Controller";
+    public static final String PII_CONTROLLER_CONNECTOR_CATEGORY = "Consent Management";
+    public static final String PII_CONTROLLER_CONNECTOR_SUB_CATEGORY = "DEFAULT";
+
+}
---
+++ consent-mgt/piicontroller/org.wso2.carbon.identity.piicontroller/src/main/java/org/wso2/carbon/identity/piicontroller/internal/IdentityPIIControllerServiceComponent.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.identity.piicontroller.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.identity.governance.IdentityGovernanceService;
+import org.wso2.carbon.identity.governance.common.IdentityConnectorConfig;
+import org.wso2.carbon.identity.piicontroller.connector.ConsentMgtConfigImpl;
+
+/**
+ * @scr.component name="IdentityPIIControllerServiceComponent" immediate="true"
+ * @scr.reference name="IdentityGovernanceService"
+ * interface="org.wso2.carbon.identity.governance.IdentityGovernanceService" cardinality="1..1"
+ * policy="dynamic" bind="setIdentityGovernanceService" unbind="unsetIdentityGovernanceService"
+ */
+public class IdentityPIIControllerServiceComponent {
+
+    private static Log log = LogFactory.getLog(IdentityPIIControllerServiceComponent.class);
+
+    protected void activate(ComponentContext context) {
+
+        try {
+            BundleContext bundleContext = context.getBundleContext();
+            bundleContext.registerService(IdentityConnectorConfig.class.getName(), new ConsentMgtConfigImpl(), null);
+        } catch (Exception e) {
+            log.error("Error while activating pii controller sample component.", e);
+        }
+    }
+
+    protected void deactivate(ComponentContext context) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("PII controller bundle is de-activated");
+        }
+    }
+
+    protected void unsetIdentityGovernanceService(IdentityGovernanceService idpManager) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Identity Governance service is unset from PII Controller sample");
+        }
+    }
+
+    protected void setIdentityGovernanceService(IdentityGovernanceService idpManager) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Identity Governance service is set form PII Controller sample");
+        }
+    }
+}
