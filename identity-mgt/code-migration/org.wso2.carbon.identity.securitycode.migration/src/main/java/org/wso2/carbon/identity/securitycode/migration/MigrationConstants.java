77fe75e07737d485e7f3830a929f4d456bdc0ddc
isura <isura@wso2.com>
2018-03-11 18:31:54 -0700

    Add security codes migration service from old soap service to compatiable with new rest service.

---
+++ identity-mgt/code-migration/org.wso2.carbon.identity.securitycode.migration/src/main/java/org/wso2/carbon/identity/securitycode/migration/MigrationConstants.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations und
+ */
+package org.wso2.carbon.identity.securitycode.migration;
+
+/**
+ * This class is used to define the constants used in security code migration.
+ */
+public class MigrationConstants {
+
+    public static final String CONFIRMATION_REGISTRY_RESOURCE_PATH = "/repository/components/org.wso2.carbon" +
+            ".identity.mgt/data/";
+    public static final String EXPIRE_TIME_PROPERTY = "expireTime";
+    public static final String USER_ID_PROPERTY = "userId";
+    public static final String SELF_SIGN_UP_CODE_DELIMITER = "1";
+    public static final String PW_RESET_CODE_DELIMITER = "2";
+    public static final String REG_DELIMITER = "___";
+    public static final String TENANT_DELIMITER = ":";
+    public static final String SYSTEM_PROPERTY_MIGRATE = "migrateSecurityCode";
+    public static final String SYSTEM_PROPERTY_TENANTS = "tenantsToMigrate";
+
+
+}
