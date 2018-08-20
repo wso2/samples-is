e153d3354762559be416702cfc725c89a38aff6c
Dileesha <dilee.dev@gmail.com>
2018-08-20 12:00:55 +0530

    Remove Pods

--- oidc-client-app-samples/ios-client-app-sample/Pods/Target Support Files/Pods-WSO2-IS-SampleApp/Pods-WSO2-IS-SampleApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WSO2_IS_SampleApp : NSObject
-@end
-@implementation PodsDummy_Pods_WSO2_IS_SampleApp
-@end
--- oidc-client-app-samples/ios-client-app-sample/Pods/Target Support Files/Pods-WSO2-IS-SampleAppTests/Pods-WSO2-IS-SampleAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WSO2_IS_SampleAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_WSO2_IS_SampleAppTests
-@end
