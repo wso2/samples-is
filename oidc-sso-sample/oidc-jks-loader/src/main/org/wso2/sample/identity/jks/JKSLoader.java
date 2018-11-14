d9c280d22b76d8e28e1d638d0de434ce34ec3ccd
Kavindu-dodan <kavindudodanduwa@gmail.com>
2018-11-14 14:33:38 +0530

    Review changes for PR
    + Adding missing pom
    + Adding missing license header
    + Dependency and plugin improvements

--- oidc-sso-sample/oidc-jks-loader/src/main/org/wso2/sample/identity/jks/JKSLoader.java
+++ oidc-sso-sample/oidc-jks-loader/src/main/org/wso2/sample/identity/jks/JKSLoader.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wso2.sample.identity.jks;
 
 import javax.servlet.ServletContextEvent;
@@ -9,6 +24,10 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A listener to get invoked at application deployment.
+ * This will allow us to set the carbon JKS for HTTPS communication.
+ * */
 public class JKSLoader implements ServletContextListener {
 
     private static final Logger LOGGER = Logger.getLogger(JKSLoader.class.getName());
