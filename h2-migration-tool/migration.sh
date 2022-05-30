852aceebc428d748dcbb599b18a8c9a536c799be
ashendes <desilvaashen.15@cse.mrt.ac.lk>
2022-05-30 17:02:23 +0400

    H2 migration tool issues fixed.

--- h2-migration-tool/migration.sh
+++ h2-migration-tool/migration.sh
@@ -14,32 +14,32 @@
  # specific language governing permissions and limitations
  # under the License.
 
-
 echo
 echo "Welcome to the H2 DB Migration Tool!"
 echo
 echo "Provide a directory path for the old db files and a directory path to store the new files."
 echo
-echo "Enter the path to the previous database files: eg. <file-path>/old-databases "
+echo "Enter the path to the previous database files: eg. <file-path>/old-databases, <OLD_IS_HOME>/repository/database "
 read src_dir
 echo
 echo "Enter the path to store the newly created files: eg. <file-path>/new-databases "
 read dest_dir
 
-wget https://repo1.maven.org/maven2/com/h2database/h2/2.1.210/h2-2.1.210.jar
-wget https://repo1.maven.org/maven2/com/h2database/h2/1.4.199/h2-1.4.199.jar
+wget -c https://repo1.maven.org/maven2/com/h2database/h2/2.1.210/h2-2.1.210.jar || exit 1
+wget -c https://repo1.maven.org/maven2/com/h2database/h2/1.4.199/h2-1.4.199.jar || exit 1
 
-for filepath in $src_dir/*.mv.db; do
+db_files=("$src_dir"/*.mv.db)
+for filepath in "${db_files[@]}"; do
     dbname=$(basename "$filepath" .mv.db)
 
     # Export data from old db file to backup.zip
     echo "Exporting database..."
-    java -cp h2-1.4.199.jar org.h2.tools.Script -url jdbc:h2:$src_dir/$dbname -user wso2carbon -password wso2carbon -script backup.zip -options compression zip
+    java -cp h2-1.4.199.jar org.h2.tools.Script -url "jdbc:h2:$src_dir/$dbname" -user wso2carbon -password wso2carbon -script backup.zip -options compression zip || exit 1
     rm -f $dest_dir/$dbname.mv.db
 
     # Import data from the backup.zip to the new db file
     echo "Importing data..."
-    java -cp h2-2.1.210.jar org.h2.tools.RunScript -url jdbc:h2:$dest_dir/$dbname -user wso2carbon -password wso2carbon -script ./backup.zip -options compression zip FROM_1X
+    java -cp h2-2.1.210.jar org.h2.tools.RunScript -url "jdbc:h2:$dest_dir/$dbname" -user wso2carbon -password wso2carbon -script ./backup.zip -options compression zip FROM_1X || exit 1
     rm -f backup.zip
 
     echo "$dbname migrated successfully"
