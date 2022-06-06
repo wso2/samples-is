519ca68be5c3900f1c2f7e67b92a301aaf401a0f
Dehami Koswatte <dehami.deshan@gmail.com>
2022-06-06 13:59:16 +0530

    Set federated IdP name to avoid DB errors

--- oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
+++ oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
@@ -21,6 +21,7 @@ package org.wso2.sample.identity.oauth2.grant.mobile;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
+import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkConstants;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.ResponseHeader;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
@@ -70,6 +71,12 @@ public class MobileGrant extends AbstractAuthorizationGrantHandler  {
                 AuthenticatedUser mobileUser = new AuthenticatedUser();
                 mobileUser.setUserName(tenantAwareUsername);
                 mobileUser.setTenantDomain(tenantDomain);
+                /*
+                   Set the federated IdP name as "LOCAL" to avoid constraint violation errors. If a federated user is
+                   involved with this custom grant then, the federated IdP name and the other claims should be fetched
+                   accordingly.
+                 */
+                mobileUser.setFederatedIdPName(FrameworkConstants.LOCAL_IDP_NAME);
                 mobileUser.setAuthenticatedSubjectIdentifier(mobileNumber);
                 mobileUser.setFederatedUser(true);
                 oAuthTokenReqMessageContext.setAuthorizedUser(mobileUser);
