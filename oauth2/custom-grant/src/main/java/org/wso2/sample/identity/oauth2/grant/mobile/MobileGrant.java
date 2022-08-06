1a191ef769a96820b467d31f0f59d56369105ace
Dehami Koswatte <dehami.deshan@gmail.com>
2022-08-06 09:47:20 +0530

    Address code review changes

--- oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
+++ oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
@@ -21,7 +21,6 @@ package org.wso2.sample.identity.oauth2.grant.mobile;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
-import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkConstants;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.ResponseHeader;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
@@ -66,19 +65,16 @@ public class MobileGrant extends AbstractAuthorizationGrantHandler  {
 
             if(authStatus) {
                 // if valid set authorized mobile number as grant user
-                String tenantDomain = MultitenantUtils.getTenantDomain(mobileNumber);
                 String tenantAwareUsername = MultitenantUtils.getTenantAwareUsername(mobileNumber);
-                AuthenticatedUser mobileUser = new AuthenticatedUser();
-                mobileUser.setUserName(tenantAwareUsername);
-                mobileUser.setTenantDomain(tenantDomain);
                 /*
-                   Set the federated IdP name as "LOCAL" to avoid constraint violation errors. If a federated user is
-                   involved with this custom grant then, the federated IdP name and the other claims should be fetched
-                   accordingly.
+                    Please use AuthenticatedUser.createFederateAuthenticatedUserFromSubjectIdentifier() if a federated
+                    user is involved with this custom grant.
                  */
-                mobileUser.setFederatedIdPName(FrameworkConstants.LOCAL_IDP_NAME);
-                mobileUser.setAuthenticatedSubjectIdentifier(mobileNumber);
-                mobileUser.setFederatedUser(true);
+                AuthenticatedUser mobileUser = AuthenticatedUser.createLocalAuthenticatedUserFromSubjectIdentifier(
+                        tenantAwareUsername);
+                // Set the federated IdP name if a federated user is involved with this custom grant.
+                // mobileUser.setFederatedIdPName(FrameworkConstants.LOCAL_IDP_NAME);
+
                 oAuthTokenReqMessageContext.setAuthorizedUser(mobileUser);
                 oAuthTokenReqMessageContext.setScope(oAuthTokenReqMessageContext.getOauth2AccessTokenReqDTO().getScope());
             } else{
