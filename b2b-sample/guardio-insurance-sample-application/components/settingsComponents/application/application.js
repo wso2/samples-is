7a3e059ac092d391f321b425b9258ea6b98e71fa
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-08-22 14:19:18 +0530

    add the license headers

--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/application/application.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/application/application.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2022 WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import Plus from '@rsuite/icons/Plus';
 import Trash from '@rsuite/icons/Trash';
 import { useSession } from "next-auth/react";
@@ -7,7 +25,7 @@ import config from '../../../config.json';
 import { getApplicationDetails, listApplications, patchApplication } from "./api";
 
 import { getDetailedIdentityProvider, listAllIdentityProviders } from "../identity-providers/api";
-import styles from "./app.module.css";
+import styles from "../../../styles/app.module.css";
 
 export default function Application() {
 
--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/viewUserComponent.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/viewUserComponent.js
@@ -1,4 +1,21 @@
-import React, { useEffect, useState } from 'react';
+/*
+ * Copyright (c) 2022 WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { useCallback, useEffect, useState } from 'react';
 import { Table } from 'rsuite';
 
 import styles from '../../styles/Settings.module.css';
@@ -12,20 +29,20 @@ export default function ViewUserComponent(props) {
 
     const [openUser, setOpenUser] = useState({});
 
-    const fetchData = async () => {
+    const fetchData = useCallback(async () => {
         const res = await decodeViewUsers(props.session);
         setUsers(res);
-    }
+    },[props.session])
 
     useEffect(() => {
         if (!editUserOpen) {
             fetchData()
         }
-    }, [editUserOpen]);
+    }, [editUserOpen,fetchData]);
 
     useEffect(() => {
         fetchData();
-    }, [props.session]);
+    }, [fetchData]);
 
     const { Column, HeaderCell, Cell } = Table;
 
