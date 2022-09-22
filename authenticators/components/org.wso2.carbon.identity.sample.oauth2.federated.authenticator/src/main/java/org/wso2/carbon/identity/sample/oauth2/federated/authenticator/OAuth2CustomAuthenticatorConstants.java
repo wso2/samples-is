9ae291337fe12755d9aa34a6c0d4255f8a813ed9
Avarjana <acpasavarjana@gmail.com>
2022-09-22 11:50:04 +0530

    Review changes

--- authenticators/components/org.wso2.carbon.identity.sample.oauth2.federated.authenticator/src/main/java/org/wso2/carbon/identity/sample/oauth2/federated/authenticator/OAuth2CustomAuthenticatorConstants.java
+++ authenticators/components/org.wso2.carbon.identity.sample.oauth2.federated.authenticator/src/main/java/org/wso2/carbon/identity/sample/oauth2/federated/authenticator/OAuth2CustomAuthenticatorConstants.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2022, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,9 @@
 
 package org.wso2.carbon.identity.sample.oauth2.federated.authenticator;
 
+/**
+ * Class to hold the constants for OAuth2 custom authenticator.
+ */
 public class OAuth2CustomAuthenticatorConstants {
 
     public static final String AUTHENTICATOR_NAME = "OAuth2CustomAuthenticator";
