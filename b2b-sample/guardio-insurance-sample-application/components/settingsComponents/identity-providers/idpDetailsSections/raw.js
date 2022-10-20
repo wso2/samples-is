cb0f35924b6849e75b3a9fee78e21de90c7ddd3b
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-20 14:28:56 +0530

    Update raw.js

--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/identity-providers/idpDetailsSections/raw.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/identity-providers/idpDetailsSections/raw.js
@@ -1,27 +1,34 @@
-/*
- * Copyright (c) 2022 WSO2 LLC. (https://www.wso2.com).
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *http://www.apache.org/licenses/LICENSE-2.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-import React from 'react';
+import React from "react";
 import styles from "../../../../styles/idp.module.css";
 
-export default function Raw(props) {
-  
-  return (
-    <pre className={styles.idp__item__json__pre}> {JSON.stringify(props.idpDetails, null, 2)}</pre>
-  )
+/**
+ * 
+ * @param prop - idpDetails
+ * @returns The raw section of an idp
+ */
+export default function Raw(prop) {
+
+    const { idpDetails } = prop;
+	
+    return (
+        <pre className={ styles.idp__item__json__pre }> { JSON.stringify(idpDetails, null, 2) }</pre>
+    );
 }
