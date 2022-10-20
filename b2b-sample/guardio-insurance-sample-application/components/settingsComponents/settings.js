a6bb726853838860843e53cc727db360134bd562
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-20 12:26:28 +0530

    settings section refactored

--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/settings.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/settings.js
@@ -1,118 +1,127 @@
-/*
- * Copyright (c) 2022 WSO2 LLC. (https://www.wso2.com).
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *http://www.apache.org/licenses/LICENSE-2.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
-import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
-import React, { useEffect, useState } from 'react';
-import { Button, Nav, Sidenav } from 'rsuite';
-import styles from '../../styles/Settings.module.css';
-
+import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
+import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
+import React, { useEffect, useState } from "react";
+import { Button, Nav, Sidenav } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
-import Custom500 from '../../pages/500';
-import { checkCustomization, hideBasedOnScopes } from '../../util/util/frontendUtil/frontendUtil';
-import { orgSignout } from '../../util/util/routerUtil/routerUtil';
-import HomeComponent from './homeComponet/homeComponent';
+import HomeComponent from "./homeComponet/homeComponent";
 import IdentityProviders from "./identity-providers/identity-providers";
-import LogoComponent from './logoComponent';
-import ViewUserComponent from './viewUserComponent';
+import LogoComponent from "./logoComponent";
+import ViewUserComponent from "./viewUserComponent";
+import Custom500 from "../../pages/500";
+import styles from "../../styles/Settings.module.css";
+import { checkCustomization, hideBasedOnScopes } from "../../util/util/frontendUtil/frontendUtil";
+import { orgSignout } from "../../util/util/routerUtil/routerUtil";
 
-export default function Settings(props) {
+/**
+ * 
+ * @param prop - orgId, name, session, colorTheme
+ * @returns The admin settings section. Mainly side nav bar and the section to show other settings sections.
+ */
+export default function Settings(prop) {
 
-    const SETTINGS_UI = "settings interface"
+    const { name, orgId, session, colorTheme } = prop;
 
-    const [activeKeySideNav, setActiveKeySideNav] = useState('1');
+    const [ activeKeySideNav, setActiveKeySideNav ] = useState("1");
 
     const mainPanelComponenet = (activeKey) => {
         switch (activeKey) {
-            case '1':
+            case "1":
 
-                return <HomeComponent orgName={props.name} orgId={props.orgId} session={props.session} />;
-            case '2-1':
+                return <HomeComponent orgName={ name } orgId={ orgId } session={ session } />;
+            case "2-1":
 
-                return <ViewUserComponent orgName={props.name} orgId={props.orgId} session={props.session} />;
-            case '2-3':
+                return <ViewUserComponent orgName={ name } orgId={ orgId } session={ session } />;
+            case "2-3":
 
-                return <IdentityProviders orgName={props.name} orgId={props.orgId} session={props.session} />;
-            // case '3-1':
-            //     return <Application orgName={props.name} session={session} />
+                return <IdentityProviders orgName={ name } orgId={ orgId } session={ session } />;
         }
-    }
+    };
 
     const activeKeySideNavSelect = (eventKey) => {
         setActiveKeySideNav(eventKey);
-    }
+    };
 
     useEffect(() => {
-        document.body.className = checkCustomization(props.colorTheme)
-    }, [props.colorTheme]);
+        document.body.className = checkCustomization(colorTheme);
+    }, [ colorTheme ]);
 
     return (
         <div>
-            {props.session && props.session.scope
-                ? <div className={styles.mainDiv}>
-                    <div className={styles.sideNavDiv}>
-                        <SideNavSection name={props.name} scope={props.session.scope}
-                            activeKeySideNav={activeKeySideNav} activeKeySideNavSelect={activeKeySideNavSelect} />
+            { session && session.scope
+                ? (<div className={ styles.mainDiv }>
+                    <div className={ styles.sideNavDiv }>
+                        <SideNavSection 
+                            name={ name } 
+                            scope={ session.scope }
+                            activeKeySideNav={ activeKeySideNav } 
+                            activeKeySideNavSelect={ activeKeySideNavSelect } />
                     </div>
-                    <div className={styles.mainPanelDiv}>
-                        {mainPanelComponenet(activeKeySideNav, props.session)}
+                    <div className={ styles.mainPanelDiv }>
+                        { mainPanelComponenet(activeKeySideNav, session) }
                     </div>
-                </div>
-                : <Custom500 />}
+                </div>)
+                : <Custom500 /> }
         </div>
-    )
+    );
 }
 
-function SideNavSection(props) {
+function SideNavSection(prop) {
+
+    const { name, scope, activeKeySideNav, activeKeySideNavSelect } = prop;
 
     const signOutOnClick = () => orgSignout();
 
     return (
-        <Sidenav className={styles.sideNav} defaultOpenKeys={['3', '4']}>
+        <Sidenav className={ styles.sideNav } defaultOpenKeys={ [ "3", "4" ] }>
             <Sidenav.Header>
-                <div style={{ marginTop: '35px', marginBottom: '25px' }}>
-                    <LogoComponent imageSize='small' name={props.name} />
+                <div style={ { marginBottom: "25px", marginTop: "35px" } }>
+                    <LogoComponent imageSize="small" name={ name } />
                 </div>
             </Sidenav.Header>
             <Sidenav.Body>
-                <Nav activeKey={props.activeKeySideNav}>
-                    <Nav.Item eventKey="1" icon={<DashboardIcon />}
-                        onSelect={(eventKey) => props.activeKeySideNavSelect(eventKey)}>
+                <Nav activeKey={ activeKeySideNav }>
+                    <Nav.Item 
+                        eventKey="1" 
+                        icon={ <DashboardIcon /> }
+                        onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                         Dashboard
                     </Nav.Item>
-                    <Nav.Menu eventKey="2" title="Settings" icon={<GearCircleIcon />}
-                        style={hideBasedOnScopes(props.scope)}>
-                        <Nav.Item eventKey="2-1"
-                            onSelect={(eventKey) => props.activeKeySideNavSelect(eventKey)}>
+                    <Nav.Menu 
+                        eventKey="2" 
+                        title="Settings" 
+                        icon={ <GearCircleIcon /> }
+                        style={ hideBasedOnScopes(scope) }>
+                        <Nav.Item 
+                            eventKey="2-1"
+                            onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                             Manage Users</Nav.Item>
-                        <Nav.Item eventKey="2-3"
-                            onSelect={(eventKey) => props.activeKeySideNavSelect(eventKey)}>
+                        <Nav.Item 
+                            eventKey="2-3"
+                            onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                             Identity Providers</Nav.Item>
-                        {/* <Nav.Item
-                            eventKey="3-1"
-                            onSelect={(eventKey) => props.activeKeySideNavSelect(eventKey)}>
-                            Manage Application
-                        </Nav.Item> */}
                     </Nav.Menu>
                 </Nav>
             </Sidenav.Body>
-            <div className={styles.nextButtonDiv}>
-                <Button size="lg" appearance='ghost' onClick={signOutOnClick}>Sign Out</Button>
+            <div className={ styles.nextButtonDiv }>
+                <Button size="lg" appearance="ghost" onClick={ signOutOnClick }>Sign Out</Button>
             </div>
         </Sidenav>
     );
