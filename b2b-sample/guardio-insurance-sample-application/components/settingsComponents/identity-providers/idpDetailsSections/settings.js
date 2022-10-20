8216cc71100db375bd40d1e30d181d5178cbfada
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-20 14:23:19 +0530

    Update settings.js

--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/identity-providers/idpDetailsSections/settings.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/identity-providers/idpDetailsSections/settings.js
@@ -20,104 +20,118 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Form } from "react-final-form";
 import { Button, ButtonToolbar, Loader, useToaster } from "rsuite";
 import FormSuite from "rsuite/Form";
-import { LOADING_DISPLAY_BLOCK, LOADING_DISPLAY_NONE } from "../../../../util/util/frontendUtil/frontendUtil";
-import { errorTypeDialog, successTypeDialog } from "../../../util/dialog";
-
+import SettingsFormSelection from "./settingsFormSection/settingsFormSelection";
 import styles from "../../../../styles/Settings.module.css";
 import decodeGetFederatedAuthenticators from
     "../../../../util/apiDecode/settings/identityProvider/decodeGetFederatedAuthenticators";
 import decodeUpdateFederatedAuthenticators from
     "../../../../util/apiDecode/settings/identityProvider/decodeUpdateFederatedAuthenticators";
 import { checkIfJSONisEmpty } from "../../../../util/util/common/common";
-import SettingsFormSelection from "./settingsFormSection/settingsFormSelection";
+import { LOADING_DISPLAY_BLOCK, LOADING_DISPLAY_NONE } from "../../../../util/util/frontendUtil/frontendUtil";
+import { errorTypeDialog, successTypeDialog } from "../../../util/dialog";
 
-export default function Settings(props) {
+/**
+ * 
+ * @param prop - session, idpDetails
+ * @returns The settings section of an idp
+ */
+export default function Settings(prop) {
 
-    const [loadingDisplay, setLoadingDisplay] = useState(LOADING_DISPLAY_NONE);
-    const [federatedAuthenticators, setFederatedAuthenticators] = useState({});
+    const { session, idpDetails } = prop;
+
+    const [ loadingDisplay, setLoadingDisplay ] = useState(LOADING_DISPLAY_NONE);
+    const [ federatedAuthenticators, setFederatedAuthenticators ] = useState({});
 
     const toaster = useToaster();
 
     const fetchData = useCallback(async () => {
         const res = await decodeGetFederatedAuthenticators(
-            props.session, props.idpDetails.id, props.idpDetails.federatedAuthenticators.defaultAuthenticatorId
+            session,  idpDetails.id,  idpDetails.federatedAuthenticators.defaultAuthenticatorId
         );
+
         await setFederatedAuthenticators(res);
-    }, [props])
+    }, [ session, idpDetails ]);
 
     useEffect(() => {
         fetchData();
-    }, [fetchData]);
+    }, [ fetchData ]);
 
-    const validate = values => {
-        let errors = {}
+    const validate = () => {
+        let errors = {};
+
         if (federatedAuthenticators.properties) {
             federatedAuthenticators.properties.filter((property) => {
                 if (!eval(property.key) && !eval(property.key).value) {
-                    errors[property.key] = "This field cannot be empty";
+                    errors[ property.key ] = "This field cannot be empty";
                 }
-            })
+            } );
         }
 
-        return errors
-    }
+        return errors;
+    };
 
-    const onDataSubmit = (response, form) => {
+    const onDataSubmit = (response) => {
         if (response) {
             successTypeDialog(toaster, "Changes Saved Successfully", "Idp updated successfully.");
             fetchData();
         } else {
             errorTypeDialog(toaster, "Error Occured", "Error occured while updating the Idp. Try again.");
         }
-    }
+    };
 
     const onUpdate = async (values, form) => {
         setLoadingDisplay(LOADING_DISPLAY_BLOCK);
-        decodeUpdateFederatedAuthenticators(props.session, props.idpDetails.id, federatedAuthenticators, values)
+        decodeUpdateFederatedAuthenticators( session,  idpDetails.id, federatedAuthenticators, values)
             .then((response) => onDataSubmit(response, form))
-            .finally((response) => setLoadingDisplay(LOADING_DISPLAY_NONE))
-    }
+            .finally(() => setLoadingDisplay(LOADING_DISPLAY_NONE));
+    };
 
     return (
-        <div className={styles.addUserMainDiv}>
+        <div className={ styles.addUserMainDiv }>
 
             <div>
 
                 <Form
-                    onSubmit={onUpdate}
-                    validate={validate}
+                    onSubmit={ onUpdate }
+                    validate={ validate }
 
-                    render={({ handleSubmit, form, submitting, pristine, errors, values }) => (
-                        <FormSuite layout="vertical" className={styles.addUserForm}
-                            onSubmit={handleSubmit} fluid>
+                    render={ ( { handleSubmit, submitting, pristine, errors } ) => (
+                        <FormSuite 
+                            layout="vertical" 
+                            className={ styles.addUserForm }
+                            onSubmit={ handleSubmit } 
+                            fluid>
 
-                            {federatedAuthenticators.properties
-                                ? <SettingsFormSelection federatedAuthenticators={federatedAuthenticators.properties}
-                                    templateId={props.idpDetails.templateId} />
+                            { federatedAuthenticators.properties
+                                ? (<SettingsFormSelection 
+                                    federatedAuthenticators={ federatedAuthenticators.properties }
+                                    templateId={ idpDetails.templateId } />)
                                 : null
                             }
 
                             <div className="buttons">
                                 <FormSuite.Group>
                                     <ButtonToolbar>
-                                        <Button className={styles.addUserButton} size="lg" appearance="primary"
+                                        <Button 
+                                            className={ styles.addUserButton } 
+                                            size="lg" 
+                                            appearance="primary"
                                             type="submit"
-                                            disabled={submitting || pristine || !checkIfJSONisEmpty(errors)}>
+                                            disabled={ submitting || pristine || !checkIfJSONisEmpty(errors) }>
                                             Update
                                         </Button>
                                     </ButtonToolbar>
                                 </FormSuite.Group>
                             </div>
                         </FormSuite>
-                    )}
+                    ) }
                 />
 
             </div>
 
-            <div style={loadingDisplay}>
+            <div style={ loadingDisplay }>
                 <Loader size="lg" backdrop content="User is adding" vertical />
             </div>
         </div>
-
-    )
+    );
 }
