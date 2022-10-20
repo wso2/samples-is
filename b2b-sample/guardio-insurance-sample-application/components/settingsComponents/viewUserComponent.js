3260a0dd89005a983cf2e9e1f4924b690b0bb181
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-20 12:05:36 +0530

    view user component refactored

--- b2b-sample/guardio-insurance-sample-application/components/settingsComponents/viewUserComponent.js
+++ b2b-sample/guardio-insurance-sample-application/components/settingsComponents/viewUserComponent.js
@@ -1,133 +1,158 @@
-/*
- * Copyright (c) 2022 WSO2 LLC. (https://www.wso2.com).
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *http://www.apache.org/licenses/LICENSE-2.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-import React, { useCallback, useEffect, useState } from 'react';
-import { Button, Stack, Table } from 'rsuite';
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Stack, Table } from "rsuite";
+import AddUserComponent from "./addUserComponent";
+import styles from "../../styles/Settings.module.css";
+import decodeViewUsers from "../../util/apiDecode/settings/decodeViewUsers";
+import EditUserComponent from "../editUser/editUserComponent";
+import SettingsTitle from "../util/settingsTitle";
 
-import styles from '../../styles/Settings.module.css';
-import decodeViewUsers from '../../util/apiDecode/settings/decodeViewUsers';
-import EditUserComponent from '../editUser/editUserComponent';
-import SettingsTitle from '../util/settingsTitle';
-import AddUserComponent from './addUserComponent';
+/**
+ * 
+ * @param prop - orgName, orgId, session
+ * @returns A component that will show the users in a table view
+ */
+export default function ViewUserComponent(prop) {
 
-export default function ViewUserComponent(props) {
-    const [users, setUsers] = useState([]);
-    const [editUserOpen, setEditUserOpen] = useState(false);
-    const [addUserOpen, setAddUserOpen] = useState(false);
+    const { orgName, orgId, session } = prop;
 
-    const [openUser, setOpenUser] = useState({});
+    const [ users, setUsers ] = useState([]);
+    const [ editUserOpen, setEditUserOpen ] = useState(false);
+    const [ addUserOpen, setAddUserOpen ] = useState(false);
+
+    const [ openUser, setOpenUser ] = useState({});
 
     const fetchData = useCallback(async () => {
-        const res = await decodeViewUsers(props.session);
+        const res = await decodeViewUsers(session);
+
         await setUsers(res);
-    }, [props.session])
+    }, [ session ]);
 
     useEffect(() => {
         if (!editUserOpen || !addUserOpen) {
-            fetchData()
+            fetchData();
         }
-    }, [editUserOpen, addUserOpen, fetchData]);
+    }, [ editUserOpen, addUserOpen, fetchData ]);
 
     useEffect(() => {
         fetchData();
-    }, [fetchData]);
+    }, [ fetchData ]);
 
     const { Column, HeaderCell, Cell } = Table;
 
     const closeEditDialog = () => {
         setOpenUser({});
         setEditUserOpen(false);
-    }
+    };
 
     const onEditClick = (user) => {
         setOpenUser(user);
         setEditUserOpen(true);
-    }
+    };
 
     const closeAddUserDialog = () => {
         setAddUserOpen(false);
-    }
+    };
 
-    const onAddUserClick = (user) => {
+    const onAddUserClick = () => {
         setAddUserOpen(true);
-    }
+    };
 
     return (
-        <div className={styles.tableMainPanelDiv}>
-            <EditUserComponent session={props.session} open={editUserOpen}
-                onClose={closeEditDialog} user={openUser} />
+        <div 
+            className={ styles.tableMainPanelDiv }
+        >
+            <EditUserComponent 
+                session={ session } 
+                open={ editUserOpen }
+                onClose={ closeEditDialog } 
+                user={ openUser } />
 
-            <AddUserComponent orgName={props.orgName} orgId={props.orgId} session={props.session}
-                open={addUserOpen} onClose={closeAddUserDialog} />
+            <AddUserComponent 
+                orgName={ orgName } 
+                orgId={ orgId } 
+                session={ session }
+                open={ addUserOpen } 
+                onClose={ closeAddUserDialog } />
 
-            <Stack direction="row" justifyContent="space-between">
-                <SettingsTitle title="Manage Users" subtitle="Manage users in the organisation" />
-                <Button appearance="primary" size="lg" onClick={onAddUserClick}>
+            <Stack 
+                direction="row" 
+                justifyContent="space-between">
+                <SettingsTitle 
+                    title="Manage Users" 
+                    subtitle="Manage users in the organisation" />
+                <Button 
+                    appearance="primary" 
+                    size="lg" 
+                    onClick={ onAddUserClick }>
                     Add User
                 </Button>
             </Stack>
 
             {
                 users ?
-                  <Table
-                        height={900}
-                        data={users}
+                    (<Table
+                        height={ 900 }
+                        data={ users }
                     >
-                        <Column width={200} align="center">
+                        <Column width={ 200 } align="center">
                             <HeaderCell><h6>First Name</h6></HeaderCell>
                             <Cell dataKey="firstName" />
                         </Column>
 
-                        <Column width={200} align="center">
+                        <Column width={ 200 } align="center">
                             <HeaderCell><h6>Last Name</h6></HeaderCell>
                             <Cell dataKey="familyName" />
                         </Column>
 
-                        <Column width={300} align="center">
+                        <Column width={ 300 } align="center">
                             <HeaderCell><h6>Id</h6></HeaderCell>
                             <Cell dataKey="id" />
                         </Column>
 
-                        <Column width={200} align="center">
+                        <Column width={ 200 } align="center">
                             <HeaderCell><h6>User Name</h6></HeaderCell>
                             <Cell dataKey="username" />
                         </Column>
 
-                        <Column flexGrow={2} align="center">
+                        <Column flexGrow={ 2 } align="center">
                             <HeaderCell><h6>Email</h6></HeaderCell>
                             <Cell dataKey="email" />
                         </Column>
-                        <Column flexGrow={1} align="center" fixed="right">
+                        <Column flexGrow={ 1 } align="center" fixed="right">
                             <HeaderCell><h6>Edit User</h6></HeaderCell>
 
                             <Cell>
-                                {rowData => (
+                                { rowData => (
                                     <span>
-                                        <a onClick={() => onEditClick(rowData)} style={{ cursor: 'pointer' }}> Edit </a>
+                                        <a onClick={ () => onEditClick(rowData) } style={ { cursor: "pointer" } }> 
+                                            Edit
+                                        </a>
                                     </span>
-                                )}
+                                ) }
                             </Cell>
                         </Column>
 
-                    </Table>
+                    </Table>)
                     : null
             }
         </div>
-
-    )
+    );
 }
