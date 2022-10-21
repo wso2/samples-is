48b9ec2ea88c60b0b3081fc99d2f91d3928d2f9f
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-21 12:02:50 +0530

    Update index.js

--- b2b-sample/guardio-insurance-sample-application/pages/index.js
+++ b2b-sample/guardio-insurance-sample-application/pages/index.js
@@ -18,14 +18,17 @@
 
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Button } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import LogoComponent from "../components/settingsComponents/logoComponent";
 import homeImage from "../public/internal/home.jpeg";
 import styles from "../styles/Home.module.css";
 
-import "rsuite/dist/rsuite.min.css";
-import LogoComponent from "../components/settingsComponents/logoComponent";
-
+/**
+ * 
+ * @returns - First interface of the app
+ */
 export default function Home() {
 
     const router = useRouter();
