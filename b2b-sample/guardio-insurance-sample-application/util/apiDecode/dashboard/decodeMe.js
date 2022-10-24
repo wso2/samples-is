99901643baed8b7ef6823b2384b8c86c813817c6
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-24 16:52:27 +0530

    front end updated

--- b2b-sample/guardio-insurance-sample-application/util/apiDecode/dashboard/decodeMe.js
+++ b2b-sample/guardio-insurance-sample-application/util/apiDecode/dashboard/decodeMe.js
@@ -1,25 +1,30 @@
-/*
- * Copyright (c) 2022 WSO2 LLC. (http://www.wso2.com).
+/**
+ * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *http://www.apache.org/licenses/LICENSE-2.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
 import callMe from "../../apiCall/dashboard/callMe";
 import { commonDecode } from "../../util/apiUtil/commonDecode";
-import decodeUser from "../../util/apiUtil/decodeUser";
+import { decodeUser } from "../../util/apiUtil/decodeUser";
 
+/**
+ * 
+ * @param session 
+ * @returns logged in users object. If failed `null`
+ */
 export default async function decodeMe(session) {
     try {
         var meData;
