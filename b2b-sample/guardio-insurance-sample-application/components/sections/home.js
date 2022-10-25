a9b439d78359a507a80090a27a665e836e6b4c21
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-10-25 13:30:28 +0530

    new signOut method called

--- b2b-sample/guardio-insurance-sample-application/components/sections/home.js
+++ b2b-sample/guardio-insurance-sample-application/components/sections/home.js
@@ -19,17 +19,16 @@
 import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
 import GearCircleIcon from "@rsuite/icons/legacy/GearCircle";
 import React, { useEffect, useState } from "react";
-import { Button, Loader, Nav, Sidenav } from "rsuite";
+import { Button, Nav, Sidenav } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 import DashboardSectionComponent from "./dashboardSection/dashboardSectionComponent";
 import IdpSectionComponent from "./settingsSection/idpSection/idpSectionComponent";
 import ManageUserSectionComponent from "./settingsSection/manageUserSection/manageUserSectionComponent";
 import Custom500 from "../../pages/500";
 import styles from "../../styles/Settings.module.css";
-import { checkCustomization, hideBasedOnScopes, LOADING_DISPLAY_BLOCK, LOADING_DISPLAY_NONE } from
-    "../../util/util/frontendUtil/frontendUtil";
-import { orgSignout } from "../../util/util/routerUtil/routerUtil";
+import { checkCustomization, hideBasedOnScopes } from "../../util/util/frontendUtil/frontendUtil";
 import LogoComponent from "../common/logo/logoComponent";
+import SignOutModal from "../common/signOutModal";
 
 /**
  * 
@@ -41,20 +40,20 @@ export default function Home(prop) {
 
     const { name, orgId, session, colorTheme } = prop;
 
-    const [loadingDisplay, setLoadingDisplay] = useState(LOADING_DISPLAY_NONE);
-    const [activeKeySideNav, setActiveKeySideNav] = useState("1");
+    const [ activeKeySideNav, setActiveKeySideNav ] = useState("1");
+    const [ signOutModalOpen, setSignOutModalOpen ] = useState(false);
 
     const mainPanelComponenet = (activeKey) => {
         switch (activeKey) {
             case "1":
 
-                return <DashboardSectionComponent orgName={name} orgId={orgId} session={session} />;
+                return <DashboardSectionComponent orgName={ name } orgId={ orgId } session={ session } />;
             case "2-1":
 
-                return <ManageUserSectionComponent orgName={name} orgId={orgId} session={session} />;
+                return <ManageUserSectionComponent orgName={ name } orgId={ orgId } session={ session } />;
             case "2-3":
 
-                return <IdpSectionComponent orgName={name} orgId={orgId} session={session} />;
+                return <IdpSectionComponent orgName={ name } orgId={ orgId } session={ session } />;
         }
     };
 
@@ -62,79 +61,75 @@ export default function Home(prop) {
         setActiveKeySideNav(eventKey);
     };
 
+    const signOutModalClose = () => {
+        setSignOutModalOpen(false);
+    };
+
     useEffect(() => {
         document.body.className = checkCustomization(colorTheme);
-    }, [colorTheme]);
+    }, [ colorTheme ]);
 
     return (
         <div>
-            {session && session.scope
-                ? (<div className={styles.mainDiv}>
-                    <div style={loadingDisplay}>
-                        <Loader size="lg" backdrop content="User is logging out" vertical />
-                    </div>
-                    <div className={styles.sideNavDiv}>
+            <SignOutModal session={ session } open={ signOutModalOpen } onClose={ signOutModalClose } />
+            { session && session.scope
+                ? (<div className={ styles.mainDiv }>
+                    <div className={ styles.sideNavDiv }>
                         <SideNavSection
-                            name={name}
-                            scope={session.scope}
-                            activeKeySideNav={activeKeySideNav}
-                            activeKeySideNavSelect={activeKeySideNavSelect}
-                            setLoadingDisplay={setLoadingDisplay}
-                            session={session}
-                            orgId={orgId} />
+                            name={ name }
+                            scope={ session.scope }
+                            activeKeySideNav={ activeKeySideNav }
+                            activeKeySideNavSelect={ activeKeySideNavSelect }
+                            setSignOutModalOpen={ setSignOutModalOpen } />
                     </div>
-                    <div className={styles.mainPanelDiv}>
-                        {mainPanelComponenet(activeKeySideNav, session)}
+                    <div className={ styles.mainPanelDiv }>
+                        { mainPanelComponenet(activeKeySideNav, session) }
                     </div>
                 </div>)
-                : <Custom500 />}
+                : <Custom500 /> }
         </div>
     );
 }
 
 function SideNavSection(prop) {
 
-    const { name, scope, activeKeySideNav, activeKeySideNavSelect, setLoadingDisplay, session, orgId } = prop;
+    const { name, scope, activeKeySideNav, activeKeySideNavSelect, setSignOutModalOpen } = prop;
 
-    const signOutOnClick = async () => await orgSignout(
-        session,
-        orgId,
-        () => setLoadingDisplay(LOADING_DISPLAY_BLOCK),
-        () => setLoadingDisplay(LOADING_DISPLAY_NONE));
+    const signOutOnClick = () => setSignOutModalOpen(true);
 
     return (
-        <Sidenav className={styles.sideNav} defaultOpenKeys={["3", "4"]}>
+        <Sidenav className={ styles.sideNav } defaultOpenKeys={ [ "3", "4" ] }>
             <Sidenav.Header>
-                <div style={{ marginBottom: "25px", marginTop: "35px" }}>
-                    <LogoComponent imageSize="small" name={name} />
+                <div style={ { marginBottom: "25px", marginTop: "35px" } }>
+                    <LogoComponent imageSize="small" name={ name } />
                 </div>
             </Sidenav.Header>
             <Sidenav.Body>
-                <Nav activeKey={activeKeySideNav}>
+                <Nav activeKey={ activeKeySideNav }>
                     <Nav.Item
                         eventKey="1"
-                        icon={<DashboardIcon />}
-                        onSelect={(eventKey) => activeKeySideNavSelect(eventKey)}>
+                        icon={ <DashboardIcon /> }
+                        onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                         Dashboard
                     </Nav.Item>
                     <Nav.Menu
                         eventKey="2"
                         title="Settings"
-                        icon={<GearCircleIcon />}
-                        style={hideBasedOnScopes(scope)}>
+                        icon={ <GearCircleIcon /> }
+                        style={ hideBasedOnScopes(scope) }>
                         <Nav.Item
                             eventKey="2-1"
-                            onSelect={(eventKey) => activeKeySideNavSelect(eventKey)}>
+                            onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                             Manage Users</Nav.Item>
                         <Nav.Item
                             eventKey="2-3"
-                            onSelect={(eventKey) => activeKeySideNavSelect(eventKey)}>
+                            onSelect={ (eventKey) => activeKeySideNavSelect(eventKey) }>
                             Identity Providers</Nav.Item>
                     </Nav.Menu>
                 </Nav>
             </Sidenav.Body>
-            <div className={styles.nextButtonDiv}>
-                <Button size="lg" appearance="ghost" onClick={signOutOnClick}>Sign Out</Button>
+            <div className={ styles.nextButtonDiv }>
+                <Button size="lg" appearance="ghost" onClick={ signOutOnClick }>Sign Out</Button>
             </div>
         </Sidenav>
     );
