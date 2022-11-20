f93b567444dc54623abf547e98a2bd4766a55f20
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2022-11-20 16:28:13 +0530

    dialog componet done

---
+++ b2b-sample/libs/shared/ui-components/src/lib/components/dialogComponent/dialogComponent.tsx
@@ -0,0 +1,26 @@
+
+
+
+
+
+  import styles from './dialog-component.module.css';
+  
+
+/* eslint-disable-next-line */
+export interface DialogComponentProps {
+}
+
+
+
+export function DialogComponent(props: DialogComponentProps) {
+  return (
+    <div className={styles['container']}>
+      
+      <h1>Welcome to DialogComponent!</h1>
+      
+    </div>
+  );
+};
+
+
+export default DialogComponent;
