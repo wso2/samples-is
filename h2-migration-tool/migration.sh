5acb0bb5160813643f311973e2a6a9453858175e
Chamath Samarawickrama <chamaths@wso2.com>
2023-08-14 02:36:47 +0530

    Update the h2-migration-tool for the h2-2.2.220 version

--- h2-migration-tool/migration.sh
+++ h2-migration-tool/migration.sh
@@ -13,10 +13,31 @@
  # KIND, either express or implied.  See the License for the
  # specific language governing permissions and limitations
  # under the License.
-
 echo
 echo "Welcome to the H2 DB Migration Tool!"
 echo
+echo "Please select the h2 version you are migrating from:"
+echo "h2-2.1.210 - (1)"
+echo "h2-1.4.199 - (2)"
+echo "h2-1.3.175 - (3)"
+read old_h2_version_choice
+
+case $old_h2_version_choice in
+    1)
+        old_h2_version="h2-2.1.210.jar"
+        ;;
+    2)
+        old_h2_version="h2-1.4.199.jar"
+        ;;
+    3)
+        old_h2_version="h2-1.3.175.jar"
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
+
 echo "Provide a directory path for the old db files and a directory path to store the new files."
 echo
 echo "Enter the path to the previous database files: eg. <file-path>/old-databases, <OLD_IS_HOME>/repository/database "
@@ -25,6 +46,8 @@ echo
 echo "Enter the path to store the newly created files: eg. <file-path>/new-databases "
 read dest_dir
 
+# Downloading necessary jars
+wget -c https://repo1.maven.org/maven2/com/h2database/h2/2.2.220/h2-2.2.220.jar || exit 1
 wget -c https://repo1.maven.org/maven2/com/h2database/h2/2.1.210/h2-2.1.210.jar || exit 1
 wget -c https://repo1.maven.org/maven2/com/h2database/h2/1.4.199/h2-1.4.199.jar || exit 1
 wget -c https://repo1.maven.org/maven2/com/h2database/h2/1.3.175/h2-1.3.175.jar || exit 1
@@ -33,30 +56,27 @@ db_files=("$src_dir"/*.mv.db "$src_dir"/*.h2.db)
 for filepath in "${db_files[@]}"; do
     if [[ "$filepath" == *"*.mv.db" || "$filepath" == *"*.h2.db" ]]; then
         continue
-    elif [[ "$filepath" == *".mv.db" ]]; then
-        dbname=$(basename "$filepath" .mv.db)
-        h2_1x_jar="h2-1.4.199.jar"
-    elif [[ "$filepath" == *".h2.db" ]]; then
-        dbname=$(basename "$filepath" .h2.db)
-        h2_1x_jar="h2-1.3.175.jar"
     fi
+    dbname=$(basename "$filepath")
+    dbname="${dbname%.*}"
 
     # Export data from old db file to backup.zip
     echo "Exporting database..."
-    java -cp $h2_1x_jar org.h2.tools.Script -url "jdbc:h2:$src_dir/$dbname" -user wso2carbon -password wso2carbon -script backup.zip -options compression zip || exit 1
+    java -cp $old_h2_version org.h2.tools.Script -url "jdbc:h2:$src_dir/$dbname" -user wso2carbon -password wso2carbon -script backup.zip -options compression zip || exit 1
     rm -f $dest_dir/$dbname.mv.db
 
     # Import data from the backup.zip to the new db file
     echo "Importing data..."
-    java -cp h2-2.1.210.jar org.h2.tools.RunScript -url "jdbc:h2:$dest_dir/$dbname" -user wso2carbon -password wso2carbon -script ./backup.zip -options compression zip FROM_1X || exit 1
+    java -cp h2-2.2.220.jar org.h2.tools.RunScript -url "jdbc:h2:$dest_dir/$dbname" -user wso2carbon -password wso2carbon -script ./backup.zip -options compression zip FROM_1X || exit 1
     rm -f backup.zip
 
     echo "$dbname migrated successfully"
 done
 
-rm -f h2-1.3.175.jar
-rm -f h2-1.4.199.jar
+rm -f h2-2.2.220.jar
 rm -f h2-2.1.210.jar
+rm -f h2-1.4.199.jar
+rm -f h2-1.3.175.jar
 
 echo
 echo "Database files are created at $dest_dir."
