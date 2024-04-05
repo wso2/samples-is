b889d586f5463fce8080b4e065f4f68c334b7a92
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2024-04-05 21:17:07 +0530

    [fix] Fix passkey not working properly in the petcare application

--- petcare-sample/b2c/mobile-app/petcare/app/src/main/java/com/wso2_sample/api_auth_sample/model/ui/activities/login/fragments/auth/auth_method/passkey/authenticator/passkey_data/ChallengeInfo.kt
+++ petcare-sample/b2c/mobile-app/petcare/app/src/main/java/com/wso2_sample/api_auth_sample/model/ui/activities/login/fragments/auth/auth_method/passkey/authenticator/passkey_data/ChallengeInfo.kt
@@ -1,13 +1,15 @@
 package com.wso2_sample.api_auth_sample.model.ui.activities.login.fragments.auth.auth_method.passkey.authenticator.passkey_data
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.JsonNode
 import com.wso2_sample.api_auth_sample.util.Util
 
-data class ChallengeInfo (
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ChallengeInfo(
     val requestId: String,
     val publicKeyCredentialRequestOptions: PublicKeyCredentialRequestOptions,
-    val request: Any
+    val request: Any?
 ) {
     companion object {
         fun getChallengeInfoFromChallengeString(challengeString: String): ChallengeInfo {
--- petcare-sample/b2c/mobile-app/petcare/app/src/main/java/com/wso2_sample/api_auth_sample/model/ui/activities/login/fragments/auth/auth_method/passkey/authenticator/passkey_data/PasskeyChallenge.kt
+++ petcare-sample/b2c/mobile-app/petcare/app/src/main/java/com/wso2_sample/api_auth_sample/model/ui/activities/login/fragments/auth/auth_method/passkey/authenticator/passkey_data/PasskeyChallenge.kt
@@ -1,15 +1,15 @@
 package com.wso2_sample.api_auth_sample.model.ui.activities.login.fragments.auth.auth_method.passkey.authenticator.passkey_data
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.wso2_sample.api_auth_sample.util.Util
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class PasskeyChallenge(
-    val challenge: String,
-    val allowCredentials: List<String>,
-    val timeout: Long,
-    val userVerification: String,
-    val rpId: String
+    val challenge: String?,
+    val allowCredentials: List<String>?,
+    val timeout: Long?,
+    val userVerification: String?,
+    val rpId: String?
 ) {
     override fun toString(): String {
         return Util.getJsonString(this)
