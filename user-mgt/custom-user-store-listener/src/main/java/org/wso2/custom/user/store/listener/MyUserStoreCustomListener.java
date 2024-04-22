f960b6b648a9b7f5fd2aa89fc6d7dbc74ceb1e60
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2024-04-22 15:39:43 +0530

    Update license headers.

--- user-mgt/custom-user-store-listener/src/main/java/org/wso2/custom/user/store/listener/MyUserStoreCustomListener.java
+++ user-mgt/custom-user-store-listener/src/main/java/org/wso2/custom/user/store/listener/MyUserStoreCustomListener.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c)  WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -27,7 +27,7 @@ import java.lang.String;
 import java.util.Map;
 
 /**
- *
+ * Custom User Store Listener Implementation.
  */
 public class MyUserStoreCustomListener extends AbstractUserOperationEventListener {
 
--- user-mgt/custom-user-store-listener/src/main/java/org/wso2/custom/user/store/listener/internal/CustomExtensionServiceComponent.java
+++ user-mgt/custom-user-store-listener/src/main/java/org/wso2/custom/user/store/listener/internal/CustomExtensionServiceComponent.java
@@ -1,8 +1,9 @@
 /*
- * Copyright (c)  WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
