3f6e1e4b9158524738dee5d994faaa6d8e6e0893
Achintha Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2024-05-04 02:28:14 +0530

    [chore] improve config file

--- petcare-sample/b2c/mobile-app/petcare-with-sdk/app/src/main/java/com/wso2_sample/api_auth_sample/util/Config.kt
+++ petcare-sample/b2c/mobile-app/petcare-with-sdk/app/src/main/java/com/wso2_sample/api_auth_sample/util/Config.kt
@@ -1,13 +1,36 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package com.wso2_sample.api_auth_sample.util
 
 object Config {
-    private const val BASE_URL: String = "https://10.0.2.2:9443"
-    private const val DISCOVERY_URL: String = "https://10.0.2.2:9443/oauth2/token/.well-known/openid-configuration"
-    private const val CLIENT_ID: String = "DemBfWSfjhJO2ieDeI67urt7O_0a"
+    private const val BASE_URL: String = <BASE_URL>
+    private const val DISCOVERY_URL: String = <DISCOVERY_URL>
+    private const val CLIENT_ID: String = <CLIENT_ID>
     private const val REDIRECT_URI: String = "wso2.apiauth.sample.android://login-callback"
     private const val SCOPE: String = "openid internal_login profile email"
-    private const val GOOGLE_WEB_CLIENT_ID = "1064044497616-0326pu24eaael3u9lhs6huarj935pmti.apps.googleusercontent.com"
-
+    private const val GOOGLE_WEB_CLIENT_ID = <GOOGLE_WEB_CLIENT_ID>
+    /**
+     * This value is the URL of the pet care backend server. If you are planning to use this
+     * application as a standalone application without a backend server, you must set this value to
+     * **null**, else you can set the URL of the backend server here.
+     */
+    private val DATA_SOURCE_RESOURCE_SERVER_URL: String? = null
 
     fun getAuthorizeUrl(): String {
         return "$BASE_URL/oauth2/authorize"
@@ -47,5 +70,9 @@ object Config {
 
     fun getGoogleWebClientId(): String {
         return GOOGLE_WEB_CLIENT_ID
+    }
+
+    fun getDataSourceResourceServerUrl(): String? {
+        return DATA_SOURCE_RESOURCE_SERVER_URL
     }
 }
