c3a50f63c1a49238f2104fd66a010a00e11f1bbf
Sachin-Mamoru <sachinm@wso2.com>
2024-05-04 00:22:29 +0530

    added cancel button and handled error overlay

--- petcare-sample/b2c/web-app/petdesk/web/react/public/config.js
+++ petcare-sample/b2c/web-app/petdesk/web/react/public/config.js
@@ -1,12 +1,12 @@
 window.config = {
-  baseUrl: "",
-  clientID: "",
+  baseUrl: "https://api.asgardeo.io/t/sachinm",
+  clientID: "U8BU5WbBkmBondfwjMOnd7buJRYa",
   signInRedirectURL: "http://localhost:3000",
   signOutRedirectURL: "http://localhost:3000",
   myAccountAppURL: "",
   petManagementServiceURL: "http://localhost:9090",
   billingServerURL: "http://localhost:9091",
-  salesforceServerURL: "http://localhost:9092",
+  salesforceServerURL: "https://508e6ccd-4d61-4f1b-8320-01c221bcf020-prod.e1-us-east-azure.choreoapis.dev/petdeskdemo/salesforceintegrationservice/endpoint-9092-1da/v1.0",
   scope: ["openid", "email", "profile", "acr"],
   enableOIDCSessionManagement: true
 };
